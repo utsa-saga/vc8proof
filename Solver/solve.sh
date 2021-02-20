86cdb8f4f31e3afda94d2a6758e7ae1e6515a1b4
utsa-saga <67452428+utsa-saga@users.noreply.github.com>
2021-02-19 19:30:22 -0600

    Add files via upload

--- Solver/solve.sh
+++ Solver/solve.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd ../Unsolved/
-g++ generatecases.cpp -o generatecases
-./generatecases
-cd ../Solver/
 g++ realizabilityChecker.cpp -Ofast -o realizabilityChecker
 for i in {1..23}
 do
